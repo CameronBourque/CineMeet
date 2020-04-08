072409c6785858c0d4eb76712b6d5dc9a94de4a6
akshaypatel <akshaypatel@tamu.edu>
2020-04-08 08:09:19 -0500

    deleted useless file

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
-const DB_USER = 'enter user here'; // change this to your username
-const DB_PASS = 'enter pass here'; // change this to your password
+const DB_USER = ''; // change this to your username
+const DB_PASS = ''; // change this to your password
 const DB_HOST = 'csce-315-db.engr.tamu.edu';
 const DB_PORT = '5432';
 const DB_DATABASE = 'CineMeet';
