5a91b68dd36c9eb25786b55ad7f19f98b2b233d4
akshaypatel <akshaypatel@tamu.edu>
2020-04-15 20:27:44 -0500

    updated view interface and db info

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
-const DB_USER = 'yonaslwcxjnavk';
-const DB_PASS = '00310e1b7bbfd72af08efb5c08278670129ae44da49c78af814c987a1230242f';
-const DB_HOST = 'ec2-52-71-85-210.compute-1.amazonaws.com';
+const DB_USER = 'akshaypatel'; // change this to your username
+const DB_PASS = '426005071'; // change this to your password
+const DB_HOST = 'csce-315-db.engr.tamu.edu';
 const DB_PORT = '5432';
-const DB_DATABASE = 'ddn8g868l664a0';
+const DB_DATABASE = 'CineMeet';
 
 const connectionString = `postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
