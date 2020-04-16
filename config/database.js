cf54470f1779458b7dc91bb5b7d1f7b694cc0f40
akshaypatel <akshaypatel@tamu.edu>
2020-04-15 20:17:07 -0500

    updated heroku db info

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
-const DB_USER = '00310e1b7bbfd72af08efb5c08278670129ae44da49c78af814c987a1230242f'; // change this to your username
-const DB_PASS = 'yonaslwcxjnavk'; // change this to your password
+const DB_USER = 'yonaslwcxjnavk';
+const DB_PASS = '00310e1b7bbfd72af08efb5c08278670129ae44da49c78af814c987a1230242f';
 const DB_HOST = 'ec2-52-71-85-210.compute-1.amazonaws.com';
 const DB_PORT = '5432';
 const DB_DATABASE = 'ddn8g868l664a0';
